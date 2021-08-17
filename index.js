20757facf16660d3719214fc235b7cc017d0abda
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-17 12:00:22 -0300

    Instalação do nodemon

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+
+app.listen(3000);
