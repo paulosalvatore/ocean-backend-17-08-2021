755f7ae3eded91be72c19f529290715d0927ed4c
Paulo Salvatore <salvatoregames@gmail.com>
2021-08-20 09:46:44 -0300

    Endpoint Read All criado

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
+// CRUD -> Create, Read (All & Single/byId), Update, Delete
+
+// "CRUD em memória"
+
+// Lista de textos (strings)
+const lista = ["Rick Sanchez", "Morty Smith"];
+//              0               1
+
+// [GET] /personagens
+// Read All
+app.get("/personagens", function (req, res) {
+    res.send(lista);
+});
+
 app.listen(3000);
